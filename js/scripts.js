6c625b5f1ee59be80ef8d2e73703e28214429bf1
Mike Hyler <mhyler@zoho.com>
2025-04-24 18:09:28 -0400

    Added an array of objects
    Created an array of pokemon objects includes name, height, and types

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+pokemons = [
+    { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
+    { name: 'Gyarados', height: 6.5, type: ['flying', 'water'] },
+    { name: 'Gastly', height: 1.3, type: ['ghost', 'poison'] },
+    { name: 'Scizor', height: 1.8, type: ['steel', 'bug'] },
+];
