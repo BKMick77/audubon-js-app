b6c22164c1e7fca2151fffe13efee7708be0792b
Mike Hyler <mhyler@zoho.com>
2025-04-25 18:55:54 -0400

    Added Loop w/ conditional
    Added a loop to list pokemon names and height set conditional for any pokemon above 6m

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-pokemons = [
+pokemon = [
     { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
     { name: 'Gyarados', height: 6.5, type: ['flying', 'water'] },
     { name: 'Gastly', height: 1.3, type: ['ghost', 'poison'] },
     { name: 'Scizor', height: 1.8, type: ['steel', 'bug'] },
 ];
+
+for (let i = 0; i < pokemon.length; i++) {
+    //    added <br> to separate pokemon
+    document.write(
+        pokemon[i].name + ' (Height: ' + pokemon[i].height + 'm)<br>'
+    );
+    if (pokemon[i].height >= 6) {
+        document.write("That's a big Pokemon!<br>");
+    }
+}
