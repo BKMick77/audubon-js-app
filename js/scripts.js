e35d06c5f9b77a2b4641a990da184d58132d1a19
Mike Hyler <mhyler@zoho.com>
2025-05-02 09:21:30 -0400

    refactor: wrapped pokemon repository in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
-pokemon = [
-    { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
-    { name: 'Gyarados', height: 6.5, type: ['flying', 'water'] },
-    { name: 'Gastly', height: 1.3, type: ['ghost', 'poison'] },
-    { name: 'Scizor', height: 1.8, type: ['steel', 'bug'] },
-];
+let pokemonRepository = (function () {
+    let pokemon = [
+        { name: 'Pidgey', height: 0.3, type: ['flying', 'normal'] },
+        { name: 'Gyarados', height: 6.5, type: ['flying', 'water'] },
+        { name: 'Gastly', height: 1.3, type: ['ghost', 'poison'] },
+        { name: 'Scizor', height: 1.8, type: ['steel', 'bug'] },
+    ];
 
-// for (let i = 0; i < pokemon.length; i++) {
-//     let bigPokemon = pokemon[i].height >= 6 ? " - That's a big Pokemon!" : '';
-//     document.write(
-//         `${pokemon[i].name} (Height: ${pokemon[i].height}m) ${bigPokemon} <br>`
-//     );
-// }
+    function add(p) {
+        pokemon.push(p);
+    }
 
-pokemon.forEach((pokemon) => {
+    function getAll() {
+        return pokemon;
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+    };
+})();
+
+pokemonRepository.add({
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison'],
+});
+
+pokemonRepository.getAll().forEach((pokemon) => {
     let bigPokemon = pokemon.height >= 6 ? " - That's a big Pokemon!" : '';
     document.write(
         `${pokemon.name} (Height: ${pokemon.height}m) ${bigPokemon} <br>`
